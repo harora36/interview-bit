0b3b5ddf12d14f3247280f5b2e1769bb3e1d01c0
Heena <33563193+harora36@users.noreply.github.com>
2018-03-04 14:27:03 -0800

    Create count_occurence_rotated_array.py

---
+++ count_occurence_rotated_array.py
@@ -0,0 +1,46 @@
+# Given a sorted array of integers, find the number of occurrences of a given target value.
+# Your algorithm’s runtime complexity must be in the order of O(log n).
+# If the target is not found in the array, return 0
+
+class Solution:
+    # @param A : tuple of integers
+    # @param B : integer
+    # @return a integer
+    def search_first(self, A, B, i, j):
+        if i > j or j < 0:
+            return -1
+        mid = int((i + (j - i) / 2))
+        if A[mid] == B and (mid == 0 or A[mid - 1] < B):
+            return mid
+        if A[mid] < B:
+            return self.search_first(A, B, mid + 1, j)
+        else:
+            return self.search_first(A, B, i, mid - 1)
+        return -1
+
+    # @param A : tuple of integers
+    # @param B : integer
+    # @return a integer
+    def search_last(self, A, B, i, j):
+        if i > j or j < 0:
+            return -1
+        mid = int((i + (j - i) / 2))
+        if A[mid] == B and (mid == j or A[mid + 1] > B):
+            return mid
+        if A[mid] <= B:
+            return self.search_last(A, B, mid + 1, j)
+        else:
+            return self.search_last(A, B, i, mid - 1)
+        return -1
+        
+    # @param A : tuple of integers
+    # @param B : integer
+    # @return an integer
+    def findCount(self, A, B):
+        i = 0
+        j = len(A) - 1
+        start = self.search_first(A, B, i, j)
+        if start == -1:
+            return 0
+        end = self.search_last(A, B, start, j)
+        return end - start + 1
